b6114996715b7baeaa7b183a9974df2b981e3655
ayyan627 <151634091+ayyan627@users.noreply.github.com>
2023-11-23 22:15:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['994406541759', '*Ayyan*', true],
+  ['923439019639', '*Staso Lala', true], 
   [''] 
 ] //Number of owners
 
